bdeb4cc908e89b1108689a55fcb39a017b8da76f
Haque <blmi@M-C02VW07GHTDF.nordstrom.net>
2021-01-18 14:51:05 -0800

    message change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <p>
-         This is my elastic bean !
+         This is my elastic bean ! Ekramul haque
        </p>
       </header>
     </div>
